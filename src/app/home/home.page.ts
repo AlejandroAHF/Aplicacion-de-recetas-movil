6653e6b5a4a18d73c5a799c03151427510471082
AlejandroAHF <aleherfernandez@gmail.com>
2025-10-07 22:17:11 -0600

    consumo de api de recetas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCardHeader, IonCardTitle, IonCardContent, IonCol, IonGrid, IonRow, IonCard } from '@ionic/angular/standalone';
+import axios from 'axios';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule ,IonCol, IonCardContent, IonCardTitle, IonCardHeader, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCard],
 })
 export class HomePage {
+  recetas: any[] = [];
   constructor() {}
+  ngOnInit(){
+    this.obtenerRecetas();
+  }
+
+  async obtenerRecetas(){
+    try{
+      const respuesta = await axios.get('https://dummyjson.com/recipes');
+      this.recetas = respuesta.data.recipes;
+      console.log(this.recetas);
+    }catch(error){
+      console.error(error);
+    }
+  }
 }
