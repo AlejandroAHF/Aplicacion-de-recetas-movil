586c574d5ca80e18b46b2495e7b762dbb418d843
AlejandroAHF <aleherfernandez@gmail.com>
2025-10-18 23:12:25 -0600

    creacion de paginas para visualizar los detalles de la receta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCardHeader, IonCardTitle, IonCardContent, IonCol, IonGrid, IonRow, IonCard } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCardHeader, IonCardTitle, IonCardContent, IonCol, IonGrid, IonRow, IonCard, IonButton } from '@ionic/angular/standalone';
 import axios from 'axios';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [CommonModule ,IonCol, IonCardContent, IonCardTitle, IonCardHeader, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCard],
+  imports: [IonButton, CommonModule ,IonCol, IonCardContent, IonCardTitle, IonCardHeader, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCard],
 })
 export class HomePage {
   recetas: any[] = [];
-  constructor() {}
+  constructor(private router: Router) {}
   ngOnInit(){
     this.obtenerRecetas();
   }
@@ -24,5 +25,14 @@ export class HomePage {
     }catch(error){
       console.error(error);
     }
+  }
+
+  verDetalle(id: number) {
+    console.log('ID de la receta:', id);
+    
+    const recetaSeleccionada = this.recetas.find(r => r.id === id);
+    this.router.navigate(['/recipe'], {
+      state: { receta: recetaSeleccionada }
+    });
   }
 }
